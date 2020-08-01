1bd3f848d6be476d1297064fdd5a6c34633d320e
TK <wrtk007@gmail.com>
2020-08-02 01:38:22 +0900

    submit templates

--- ullehbulleh/blog/models.py
+++ ullehbulleh/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class info(models.Model) :
+    teamno = models.IntegerField
+    teamname = models.CharField(max_length='200')
+    pagetitle = models.CharField(max_length='200')
+    picture = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+    explanation = models.TextField() #140자 제한
+    pagelink = models.TextField()
--- ullehbulleh/blog/urls.py
+++ ullehbulleh/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.urls import from django.conf import settings
+from django.
 
 app_name = 'blog'
 urlpatterns = [
-    path('',예시),
+    path(''),
     path('<int:pk>')
 ]
